0edbd26611e0620b301dcc1fe200178984fa782a
qijun4tian <424767284@qq.com>
2022-12-07 22:21:49 +0800

    汉诺塔

--- js/var.js
+++ js/var.js
@@ -1,6 +1,18 @@
 'use strict';
-console.log(x);                // undefined (注意，不会抛出 ReferenceError)
-console.log('still going...'); // still going...
-var x = 1;
-console.log(x);                // 1
-console.log('still going...'); // still going...
+// console.log(x);                // undefined (注意，不会抛出 ReferenceError)
+// console.log('still going...'); // still going...
+// var x = 1;
+// console.log(x);                // 1
+// console.log('still going...'); // still going...
+
+function resolve(){
+    console.log(222)
+}
+var  a = 10;
+while (a > 0){
+    a--;
+
+    setTimeout(()=>{
+        console.log(222)
+    },500);
+}
